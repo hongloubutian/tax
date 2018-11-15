9d0648cca54191b5cd52d2d3dd83fde97d389701
ohguktong <ohguktong@sina.com>
2018-11-15 13:58:56 +0800

    更新为5000的新税率

--- PersonalTax/CalcPersonTax.cs
+++ PersonalTax/CalcPersonTax.cs
@@ -8,28 +8,29 @@ namespace PersonalTax
 {
     public static class CalcPersonTax
     {
-        public static decimal GetTaxLevel(this decimal salary,out decimal taxRate,out int fastNum)
+        public static decimal GetTaxLevel(this decimal salary, out decimal taxRate, out int fastNum)
         {
             decimal afterSalary = 0, rate = 0;
             int num = 0;
-            if (salary>0&&salary <= 1500)
+            if (salary > 0 && salary <= 3000)
             {
                 afterSalary = salary * ((Decimal)TaxRate.First / 100) - (Decimal)FastNum.First;
                 rate = (Decimal)TaxRate.First / 100;
                 num = (int)FastNum.First;
             }
-            else if (salary > 1500 && salary <= 4500)
+            else if (salary > 3000 && salary <= 12000)
             {
                 afterSalary = salary * ((Decimal)TaxRate.Second / 100) - (Decimal)FastNum.Second;
                 rate = (Decimal)TaxRate.Second / 100;
                 num = (int)FastNum.Second;
             }
-            else if (salary>4500&&salary<=9000) {
+            else if (salary > 12000 && salary <= 25000)
+            {
                 afterSalary = salary * ((Decimal)TaxRate.Third / 100) - (Decimal)FastNum.Third;
                 rate = (Decimal)TaxRate.Third / 100;
                 num = (int)FastNum.Third;
             }
-            else if (salary > 9000 && salary <= 35000)
+            else if (salary > 25000 && salary <= 35000)
             {
                 afterSalary = salary * ((Decimal)TaxRate.Forth / 100) - (Decimal)FastNum.Forth;
                 rate = (Decimal)TaxRate.Forth / 100;
@@ -47,7 +48,8 @@ namespace PersonalTax
                 rate = (Decimal)TaxRate.Sixth / 100;
                 num = (int)FastNum.Sixth;
             }
-            else if (salary > 80000) {
+            else if (salary > 80000)
+            {
                 afterSalary = salary * ((Decimal)TaxRate.Last / 100) - (Decimal)FastNum.Last;
                 rate = (Decimal)TaxRate.Last / 100;
                 num = (int)FastNum.Last;
@@ -59,23 +61,25 @@ namespace PersonalTax
         }
     }
 
-    public enum  TaxRate{
-        First=3,    //<=1500
-        Second=10,  //1500<x<=4500
-        Third=20,   //4500<x<=9000
-        Forth=25,   //9000<x<=35000
-        Fifth=30,   //35000<x<=55000
-        Sixth=35,   //55000<x<=80000
-        Last=45     //x>80000
+    public enum TaxRate
+    {
+        First = 3,    //<=3000
+        Second = 10,  //3000<x<=12000
+        Third = 20,    //12000<x<=25000
+        Forth = 25,   //25000<x<=35000
+        Fifth = 30,   //35000<x<=55000
+        Sixth = 35,   //55000<x<=80000
+        Last = 45      //x>80000
     }
 
-    public enum FastNum {
-        First=0,          //<=1500
-        Second=105,       //1500<x<=4500
-        Third=555,        //4500<x<=9000
-        Forth=1005,       //9000<x<=35000
-        Fifth=2755,       //35000<x<=55000
-        Sixth=5505,       //55000<x<=80000
-        Last= 13505        //x>80000
+    public enum FastNum
+    {
+        First = 0,          //<=3000
+        Second = 210,       //3000<x<=12000
+        Third = 1410,        //12000<x<=25000
+        Forth = 2660,       //25000<x<=35000
+        Fifth = 4410,       //35000<x<=55000
+        Sixth = 7160,       //55000<x<=80000
+        Last = 15160        //x>80000
     }
 }
